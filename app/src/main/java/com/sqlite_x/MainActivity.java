067893f61de60d0c30c407b0253aa82dfefc510e
Marko <ms7538@gmail.com>
2018-04-10 22:42:35 -0600

    working example!

--- app/src/main/java/com/sqlite_x/MainActivity.java
+++ app/src/main/java/com/sqlite_x/MainActivity.java
@@ -2,12 +2,46 @@ package com.sqlite_x;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText textEdit;
+    TextView textView;
+    MyDBHandler dbHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textEdit = findViewById(R.id.editText);
+        textView = findViewById(R.id.dbText);
+        dbHandler = new MyDBHandler(this, null, null, 1);
+        printDatabase();
+
     }
+
+    public void addButtonClicked(View view) {
+        Products product = new Products(textEdit.getText().toString());
+        dbHandler.addProduct(product);
+        printDatabase();
+    }
+    public void deleteButtonClicked(View view){
+        String inputText = textEdit.getText().toString();
+        dbHandler.deleteProduct(inputText);
+        printDatabase();
+    }
+
+    public void printDatabase() {
+
+        String dbString = dbHandler.databaseToString();
+        textView.setText(dbString);
+        textEdit.setText("");
+    }
+
+
 }
--- app/src/main/java/com/sqlite_x/MyDBHandler.java
+++ app/src/main/java/com/sqlite_x/MyDBHandler.java
@@ -7,7 +7,7 @@ import android.content.Context;
 import android.content.ContentValues;
 
 
-public class MyDBHandler extends SQLiteOpenHelper{
+public class MyDBHandler extends SQLiteOpenHelper {
 
     private static final int DATABASE_VERSION = 1;
     private static final String DATABASE_NAME = "products.db";
@@ -31,6 +31,39 @@ public class MyDBHandler extends SQLiteOpenHelper{
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        db.execSQL(" DROP TABLE IF EXISTS " + TABLE_PRODUCTS);
+        onCreate(db);
     }
+
+    public void addProduct(Products product) {
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_PRODUCTNAME, product.get_productname());
+        SQLiteDatabase db = getWritableDatabase();
+        db.insert(TABLE_PRODUCTS, null, values);
+        db.close();
+    }
+
+    public void deleteProduct(String productName) {
+        SQLiteDatabase db = getWritableDatabase();
+        db.execSQL("DELETE FROM " + TABLE_PRODUCTS + " WHERE " + COLUMN_PRODUCTNAME + "=\""
+        + productName + "\";");
+    }
+    public String databaseToString(){
+
+        String dbString = "";
+        SQLiteDatabase db = getWritableDatabase();
+        String query = "SELECT * FROM " + TABLE_PRODUCTS + " WHERE 1";
+        Cursor c = db.rawQuery(query, null);
+        c.moveToFirst();
+        while(!c.isAfterLast()){
+            if(c.getString(c.getColumnIndex("productname")) != null){
+                dbString += c.getString(c.getColumnIndex("productname"));
+                dbString += "\n";
+                }
+            c.moveToNext();
+            }
+        db.close();
+        return dbString;
+    }
+
 }
